54bae5ffd6aeec3617459b26fc70546d298b3775
Carsten Zimmermann <cz@aegisnet.de>
2014-12-03 15:49:18 +0100

    Read from input, write to file

--- spec/command_line_spec.rb
+++ spec/command_line_spec.rb
@@ -2,8 +2,14 @@ require 'spec_helper'
 require 'open3'
 
 describe 'the command line interface' do
+  let(:inputfile) { File.join File.dirname(File.expand_path(__FILE__)), 'response.xml' }
+  let(:response)  { File.read(inputfile) }
 
-  context 'called without arguments' do
+  before do
+    stub_request(:get, "http://api.sistrix.net/*").to_return(body: response)
+  end
+
+  context 'called with insuffient arguments' do
     it 'displays usage information' do
       output, status = run_cli
       status.wont_equal 0
@@ -11,8 +17,33 @@ describe 'the command line interface' do
     end
   end
 
-  def run_cli(input = '')
-    output, status = Open3.capture2('./bin/tisriss', stdin_data: '')
+  context 'with persisted input' do
+    it 'transforms the input to stdout' do
+      output, status = run_cli(args: ["--input #{inputfile}"])
+
+      output.must_match(/\A<html>/)
+    end
+
+    it 'writes transformed data to file' do
+      outputfile = "./tix.html"
+
+      output, status = run_cli(args: ["--input #{inputfile}", "--file #{outputfile}"])
+
+      output.must_equal ''
+      File.exists?(outputfile).must_equal true
+      File.read(outputfile).must_match(/\A<html>/)
+
+      FileUtils.rm outputfile
+    end
+
+    it 'allows piping of input data' do
+      skip 'Feature not yet implemented'
+    end
+
+  end
+
+  def run_cli(input = '', args: [])
+    output, status = Open3.capture2("./bin/tisriss #{args.join(' ')}", stdin_data: '')
     [output, status.to_i]
   end
 end
